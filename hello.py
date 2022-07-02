6cac4b43e8bb01d0a30eab2ca7a1cce1c2d08bbd
harapham <thanhtrang.230402@gmail.com>
2022-07-02 23:55:00 +0700

    hara

--- hello.py
+++ hello.py
@@ -1,9 +1,1 @@
-my_string = "đây là một chuỗi"
-
-# Sử dụng hàm title() của lớp string
-new_string = my_string.title()
-
-print(new_string)
-
-# kết quả
-# Đây Là Một Chuỗi
+print("Goodbye, World!")
